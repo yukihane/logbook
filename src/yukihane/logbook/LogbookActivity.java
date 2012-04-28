1f700cac509f7e6d3e6c48266643d69de83bba36
yukihane <yukihane.feather@gmail.com>
2012-04-28 13:30:48 +0900

    feed読み込み処理

--- src/yukihane/logbook/LogbookActivity.java
+++ src/yukihane/logbook/LogbookActivity.java
@@ -1,15 +1,27 @@
 package yukihane.logbook;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
 import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
 import com.facebook.android.Facebook.DialogListener;
 import com.facebook.android.FacebookError;
+import com.facebook.android.R.id;
+import com.facebook.android.Util;
 
 public class LogbookActivity extends Activity {
     public static final String TAG = "LOGBOOK";
@@ -17,6 +29,7 @@ public class LogbookActivity extends Activity {
     private static final String ACCESS_TOKEN = "access_token";
     private static final String ACCESS_EXPIRES = "access_expires";
     private final Facebook facebook = new Facebook(FBAPP_ID);
+    private final AsyncFacebookRunner runner = new AsyncFacebookRunner(facebook);
     private SharedPreferences mPrefs;
 
     /** Called when the activity is first created. */
@@ -26,6 +39,8 @@ public class LogbookActivity extends Activity {
         setContentView(R.layout.main);
 
         initSession();
+
+        runner.request("me/feed", new MeRequestListener());
     }
 
     private void initSession() {
@@ -84,6 +99,60 @@ public class LogbookActivity extends Activity {
             // TODO Auto-generated method stub
             Log.v(TAG, "onCancel");
 
+        }
+    }
+
+    private class MeRequestListener implements RequestListener {
+
+        @Override
+        public void onComplete(String response, Object state) {
+            // TODO Auto-generated method stub
+            Log.v(TAG, "MeRequestListener#onComplete");
+            try {
+                final JSONObject res = Util.parseJson(response);
+                final String text = res.toString(1);
+                Log.i(TAG, text);
+                LogbookActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final TextView view = (TextView) findViewById(id.myview);
+                        view.setText(text);
+                    }
+                });
+//                final List<? extends Drawable> drawables = Feed.fromJSONObject(res);
+            } catch (FacebookError e) {
+                // TODO Auto-generated catch block
+                Log.e(TAG, "MeRequestListener#onComplete", e);
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                Log.e(TAG, "MeRequestListener#onComplete", e);
+            }
+            Log.i(TAG, "" + state);
+
+        }
+
+        @Override
+        public void onIOException(IOException e, Object state) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "MeRequestListener#onIOException", e);
+        }
+
+        @Override
+        public void onFileNotFoundException(FileNotFoundException e, Object state) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "MeRequestListener#onComplete", e);
+        }
+
+        @Override
+        public void onMalformedURLException(MalformedURLException e, Object state) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "MeRequestListener#onComplete", e);
+        }
+
+        @Override
+        public void onFacebookError(FacebookError e, Object state) {
+            // TODO Auto-generated method stub
+            Log.e(TAG, "MeRequestListener#onComplete", e);
         }
     }
 }
