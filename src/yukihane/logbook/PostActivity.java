a47b1ca7b3844f9b89d647596338e64b805daf50
yukihane <yukihane.feather@gmail.com>
2012-05-03 20:09:20 +0900

    option menuから書き込み用アクティビティを起動

---
+++ src/yukihane/logbook/PostActivity.java
@@ -0,0 +1,14 @@
+package yukihane.logbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post);
+    }
+
+}
