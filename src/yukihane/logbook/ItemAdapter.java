e84ceb24801a0c2af6ca431458c599a6abca3aca
yukihane <yukihane.feather@gmail.com>
2012-05-15 22:19:24 +0900

    import編成

--- src/yukihane/logbook/ItemAdapter.java
+++ src/yukihane/logbook/ItemAdapter.java
@@ -2,11 +2,8 @@ package yukihane.logbook;
 
 import static yukihane.logbook.LogbookApplication.TAG;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 import java.util.TreeSet;
 
 import yukihane.logbook.entity.Listable;
